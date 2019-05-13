e364615f7e529de6451363507a652203dedff991
Castjuanito <juanario1997@gmail.com>
2019-05-13 09:33:53 -0500

    Orden mensajes socket

--- arduino/ArduinoController.js
+++ arduino/ArduinoController.js
@@ -10,7 +10,7 @@ module.exports = {
 function iniciar() {
     var io = require('./SocketController');
     var five = require("johnny-five"),
-        board, buttonL, buttonR, buttonStart,slider;
+        board, buttonL, buttonR, buttonStart, sensor;
     var SocketController = io.getIo();
 
     board = new five.Board();
@@ -23,7 +23,7 @@ function iniciar() {
         buttonL = new five.Button(2);
         buttonR = new five.Button(4);
         buttonStart = new five.Button(6);
-        slider = new five.Sensor("A0");
+        sensor = new five.Sensor("A0");
 
 
         // Inject the `button` hardware into
@@ -44,62 +44,58 @@ function iniciar() {
         // "down" the button is pressed
         buttonL.on("down", function () {
             console.log("Ldown");
-            SocketController.emit('messages', "Ldown");
+            SocketController.emit('Left', "down");
         });
 
-        // "hold" the button is pressed for specified time.
-        //        defaults to 500ms (1/2 second)
-        //        set
-        buttonL.on("hold", function () {
-            console.log("Lhold");
-            SocketController.emit('messages', "Lhold");
-        });
 
         // "up" the button is released
         buttonL.on("up", function () {
             console.log("Lup");
-            SocketController.emit('messages', "Lup");
+            SocketController.emit('Left', "up");
         });
 
         // "down" the button is pressed
         buttonR.on("down", function () {
             console.log("Rdown");
-            SocketController.emit('messages', "Rdown");
-        });
-
-        // "hold" the button is pressed for specified time.
-        //        defaults to 500ms (1/2 second)
-        //        set
-        buttonR.on("hold", function () {
-            console.log("Rhold");
-            SocketController.emit('messages', "Rhold");
+            SocketController.emit('Right', "down");
         });
 
         // "up" the button is released
         buttonR.on("up", function () {
             console.log("Rup");
-            SocketController.emit('messages', "Rup");
+            SocketController.emit('Right', "up");
         });
 
 
         // "down" the button is pressed
         buttonStart.on("down", function () {
             console.log("Startdown");
-            SocketController.emit('messages', "Startdown");
+            SocketController.emit('Start', "down");
         });
 
+        var anteriorPot = 0; 
+        var disp = false;
 
-        // "up" the button is released
-        buttonStart.on("up", function () {
-            console.log("Startup");
-            SocketController.emit('messages', "Startup");
+
+        // Scale the sensor's data from 0-1023  log changes
+        sensor.on("change", function () {
+            var ant = this.scaleTo(0, 1023)-2;
+            var des = this.scaleTo(0, 1023)+2;
+            
+            if (anteriorPot<ant||anteriorPot>des) {
+                if (anteriorPot>des+1&&disp==false) {
+                    console.log(this.scaleTo(0, 1023));
+                    SocketController.emit('Disparo',this.scaleTo(0, 1023) );
+                    disp=true;  
+                }if (anteriorPot<des) {
+                    disp=false;
+                }
+                anteriorPot=this.scaleTo(0, 1023);
+            }
         });
-
-        slider.scale([0, 100]).on("slide", function() {
-            console.log("slide", this.value);
-          });
     });
 
     // aaaaas
 
 }
+
